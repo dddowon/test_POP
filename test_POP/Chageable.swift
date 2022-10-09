035e0512201b65a5870113a0ef77881092662542
dddowon <dowon4095@naver.com>
2022-10-09 19:10:53 +0900

    feat: Chargeable 프로토콜 추가

---
+++ test_POP/Chageable.swift
@@ -0,0 +1,19 @@
+//
+//  Chageable.swift
+//  test_POP
+//
+//  Created by 박도원 on 2022/10/09.
+//
+
+import Foundation
+
+import Foundation
+
+typealias WattPerHour = Int
+typealias Watt = Int
+
+protocol Chargeable {
+    var maximumWattPerHour: WattPerHour { get }
+    
+    func convert(chargeableWattPerHour: WattPerHour) -> WattPerHour
+}
---
+++ test_POP/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  test_POP
+//
+//  Created by 박도원 on 2022/10/09.
+//
+
+import Foundation
+
+
